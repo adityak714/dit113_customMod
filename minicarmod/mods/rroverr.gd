e7d1895a39a672c30d8187b2650ccc8c5e79a585
Aditya Khadkikar <91271297+adityak714@users.noreply.github.com>
2022-03-23 14:04:01 +0100

    Add files via upload

---
+++ minicarmod/mods/rroverr.gd
@@ -0,0 +1,15 @@
+extends Reference
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var mod_name: String = "example"
+
+# Called when the node enters the scene tree for the first time.
+func init(global) -> void:
+	print("Hello World!")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
