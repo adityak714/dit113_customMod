c67b1daef39ab35596e50a2ff1069199b67ae94d
adityaa <adityaa@student.chalmers.se>
2022-03-24 00:48:17 +0100

    tried making a custom env.. need to improve by having better surface on which the car moves .. need to make a new .tscn file

--- minicarmod/mods/rroverr.gd
+++
@@ -1,7 +0,0 @@
-extends Reference
-
-var mod_name: String = "example"
-
-func init(global) -> void:
-	global.register_environment("RestaurantSpicySalmon", preload("res://RestaurantSpicySalmon.tscn"))	
-	print("Hello World!")
