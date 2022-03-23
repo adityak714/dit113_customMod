5c1576076f4440b670a7df544eb2ac529d85e6a1
Aditya Khadkikar <91271297+adityak714@users.noreply.github.com>
2022-03-23 14:28:14 +0100

    add cmd to register trial mod

--- minicarmod/mods/rroverr.gd
+++ minicarmod/mods/rroverr.gd
@@ -3,4 +3,5 @@ extends Reference
 var mod_name: String = "example"
 
 func init(global) -> void:
+	global.register_environment("RestaurantSpicySalmon", preload("res://RestaurantSpicySalmon.tscn"))	
 	print("Hello World!")
