d650c97a73437f49d9796ff5712f5c5899f6562b
Aditya Khadkikar <adityakhadkikar@gmail.com>
2022-04-05 00:09:37 +0200

    add vehicle mod PPMobile

--- minicarCustomMod/mods/rroverr.gd
+++ minicarCustomMod/mods/rroverr.gd
@@ -9,6 +9,7 @@ var mod_name: String = "example"
 # Called when the node enters the scene tree for the first time.
 func init(global) -> void:
 	global.register_environment("example/Example", preload("res://RestaurantSpicySalmon.tscn"))
+	global.register_environment("PPMobile", preload("res://vehicle/PPMobile.tscn"))
 	print("Hello World!")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
